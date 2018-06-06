42d8f75a4555cd25a8253916f548c3b9b22e71b5
Aliska <aliska2602@yandex.ru>
2018-06-06 23:34:41 +0300

    fix third..

--- task3.js
+++ task3.js
@@ -1,7 +1,7 @@
 var input_string = process.argv[process.argv.length - 1];
 var numbers_list = "1234567890";
-var vowel_list = "aeiouy";
-var consonant_list = "bcdfghjklmnpqrstvwxz";
+var vowel_list = "aeiou";
+var consonant_list = "bcdfghjklmnpqrstvwxyz";
 
 var splitString = function (input_string) {
     var length = input_string.length;
